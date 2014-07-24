ebf122833a20e99749395bfa8c7c41a41b47886e
Ryohei Suda <m5161129@u-aizu.ac.jp>
2014-07-24 18:09:36 +0900

    Added error messages if images cannnot open

--- CornerDetection/main.cpp
+++ CornerDetection/main.cpp
@@ -25,10 +25,22 @@ int main(int argc, const char * argv[])
     std::vector<CornerDetection::pair>::iterator pair = cd.image_names.begin();
     for (; pair != cd.image_names.end(); ++pair) {
         cv::Mat white = cv::imread(pair->white, CV_LOAD_IMAGE_GRAYSCALE);
+        if (white.empty()) {
+            std::cerr << "Cannot open " << pair->white << std::endl;
+        }
         cv::Mat black = cv::imread(pair->black, CV_LOAD_IMAGE_GRAYSCALE);
+        if (black.empty()) {
+            std::cerr << "Cannot open " << pair->black << std::endl;
+        }
     
         cv::Mat pattern1 = cv::imread(pair->pattern1, CV_LOAD_IMAGE_GRAYSCALE);
+        if (pattern1.empty()) {
+            std::cerr << "Cannot open " << pair->pattern1 << std::endl;
+        }
         cv::Mat pattern2 = cv::imread(pair->pattern2, CV_LOAD_IMAGE_GRAYSCALE);
+        if (pattern2.empty()) {
+            std::cerr << "Cannot open " << pair->pattern2 << std::endl;
+        }
         cv::Mat mask = cd.makeMask(white, black);
       
         cv::Canny(pattern1, pattern1, 150, 400);
