2c0b9c4ce57b91de9c30ad5dfc3dd7c5709eee5e
Ryohei Suda <m5161129@u-aizu.ac.jp>
2014-10-26 21:04:06 +0900

    Changed name from CameraMovement to CameraMotion

---
+++ CameraMotion/CameraMotion.cpp
@@ -0,0 +1,10 @@
+//
+//  CameraMotion.cpp
+//  CameraMotion
+//
+//  Created by Ryohei Suda on 2014/10/26.
+//  Copyright (c) 2014年 RyoheiSuda. All rights reserved.
+//
+
+#include "CameraMotion.h"
+
--- CameraMotion/main.cpp
+++ CameraMotion/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  CameraMovement
+//  CameraMotion
 //
 //  Created by Ryohei Suda on 2014/10/26.
 //  Copyright (c) 2014年 RyoheiSuda. All rights reserved.
