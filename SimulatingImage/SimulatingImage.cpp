381c08554cf44fc8755c229df2ab21fd2811ea26
Ryohei Suda <m5161129@u-aizu.ac.jp>
2014-11-25 23:40:06 +0900

    Created new project, SimulatingImage

---
+++ SimulatingImage/SimulatingImage.cpp
@@ -0,0 +1,9 @@
+//
+//  SimulatingImage.cpp
+//  SimulatingImage
+//
+//  Created by Ryohei Suda on 2014/11/25.
+//  Copyright (c) 2014年 RyoheiSuda. All rights reserved.
+//
+
+#include "SimulatingImage.h"
---
+++ SimulatingImage/SimulatingImage.h
@@ -0,0 +1,14 @@
+//
+//  SimulatingImage.h
+//  SimulatingImage
+//
+//  Created by Ryohei Suda on 2014/11/25.
+//  Copyright (c) 2014年 RyoheiSuda. All rights reserved.
+//
+
+#ifndef __SimulatingImage__SimulatingImage__
+#define __SimulatingImage__SimulatingImage__
+
+#include <stdio.h>
+
+#endif /* defined(__SimulatingImage__SimulatingImage__) */
