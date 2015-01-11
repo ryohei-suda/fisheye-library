ef6fa01b6fbdb793e6b26e8e1862f2f7d735f516
Ryohei Suda <m5161129@u-aizu.ac.jp>
2015-01-11 19:22:25 +0900

    Created a new project, Evaluation

---
+++ Evaluation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Evaluation
+//
+//  Created by Ryohei Suda on 2015/01/11.
+//  Copyright (c) 2015年 RyoheiSuda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
