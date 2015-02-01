3a48f70132651639b87f04ef999e95c254bc6f51
Ryohei Suda <m5161129@u-aizu.ac.jp>
2015-02-01 11:16:47 +0900

    Created a project, Reconstruction

---
+++ Reconstruction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Reconstruction
+//
+//  Created by Ryohei Suda on 2015/02/01.
+//  Copyright (c) 2015年 RyoheiSuda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
